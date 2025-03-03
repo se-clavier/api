a60931a4d021025c0c0a3001b1753c7ce49d9999
CoderOJ <coderoj@outlook.com>
2025-03-03 11:58:20 +0800

    arch: associate Request and Response types

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,32 @@
 use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
-#[derive(JsonSchema)]
+#[derive(JsonSchema, Serialize, Deserialize)]
 pub struct User {
 	pub id: u64,
 	pub name: String,
 }
 
-#[derive(JsonSchema)]
-pub struct LoginRequest {
+#[derive(JsonSchema, Serialize, Deserialize)]
+pub struct AuthRequest {
 	pub username: String,
 	pub password: String,
 }
 
-#[derive(JsonSchema)]
-pub struct LoginResponse {
+#[derive(JsonSchema, Serialize, Deserialize)]
+pub struct AuthResponse {
 	pub user: User,
 	pub token: String,
 }
 
-#[derive(JsonSchema)]
-pub enum API {
-	LoginRequest(LoginRequest),
-	LoginResponse(LoginResponse),
+// use enum instead of trait to support JsonSchema and typescript
+#[derive(JsonSchema, Serialize, Deserialize)]
+pub enum API<Request, Response> {
+	Call(Request),
+	Return(Response),
+}
+
+#[derive(JsonSchema, Serialize, Deserialize)]
+pub enum APICollection {
+	Auth(API<AuthRequest, AuthResponse>),
 }
