60f04f71500d86f815858c0bc0bc6605b194e879
asd-a <hjh-asd@outlook.com>
2025-03-20 14:38:24 +0800

    feat: add adjust api

--- api.rkt
+++ api.rkt
@@ -33,6 +33,47 @@
       `[password string])
     'LoginResponse)
   
+  (type 'Time
+    `[week uuid]
+    `[day uuid]
+    `[num uuid])
+  
+  (api 'user_queue
+    #:auth 'user
+    (type 'UserQueueRequest
+    `[room_id uuid]
+    `[time Time])
+    (type `UserQueueResponse
+      `[success bool]
+      `[rank uuid]))
+      
+  (api 'user_cancel
+    #:auth 'user
+    (type 'UserCancelRequest
+    `[room_id uuid]
+    `[time Time])
+    (type `UserCancelResponse
+      `[success bool]))
+  
+  (api 'admin_queue
+    #:auth 'admin
+    (type 'AdminQueueRequest
+    `[room_id uuid]
+    `[time Time]
+    `[user_id uuid])
+    (type `AdminQueueResponse
+      `[success bool]
+      `[rank uuid]))
+      
+  (api 'admin_cancel
+    #:auth 'admin
+    (type 'AdminCancelRequest
+    `[room_id uuid]
+    `[time Time]
+    `[user_id uuid])
+    (type `AdminCancelResponse
+      `[success bool]))
+
   (api 'test_auth_echo
     #:auth 'user 
     (type `TestAuthEchoRequest
