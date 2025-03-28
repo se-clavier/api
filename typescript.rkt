d6550252d06ec46d9f028d1072528217bde1efbe
CoderOJ <yangzhan23@mails.tsinghua.edu.cn>
2025-03-28 22:35:31 +0800

    upd(typescript): use get_auth instead of get_login (#21)

--- typescript.rkt
+++ typescript.rkt
@@ -78,18 +78,13 @@
   ; Generate api call
   (printf "export class API {
     private _fetch;
-    constructor(fetch: (req: APICollection) => Promise<any>, get_login: () => Promise<LoginRequest>) {
+    constructor(fetch: (req: APICollection) => Promise<any>, get_auth: () => Promise<Auth>) {
       let auth: Auth | undefined = undefined
       this._fetch = async (req_unauth: (auth: () => Promise<Auth>) => Promise<APICollection>,
-                           refresh_auth: boolean = false): Promise<any> => {
+        refresh_auth: boolean = false): Promise<any> => {
         const req = await req_unauth(async () => {
           if (!auth || refresh_auth) {
-            let login_res = await this.login(await get_login())
-            if (login_res.type !== 'Success') {
-              throw new Error(login_res.type)
-            } else {
-              auth = login_res
-            }
+            auth = await get_auth()
           }
           return auth;
         })
