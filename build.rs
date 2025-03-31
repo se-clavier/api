195a40b70b1e3b72e84fbd320c7e6ff4ca2c1527
asd-a <asdro.huang@outlook.com>
2025-03-31 18:40:16 +0800

    upd(rust-build)

--- build.rs
+++ build.rs
@@ -2,9 +2,10 @@ use std::{fs::File, process::Command};
 
 fn main() {
     let status = Command::new("racket")
-            .arg("rust.rkt")
-            .stdout(File::create("src/api.rs").expect("failed to create src/api.rs"))
-            .status().expect("failed to run racket");
+        .arg("rust.rkt")
+        .stdout(File::create("src/api.rs").expect("failed to create src/api.rs"))
+        .status()
+        .expect("failed to run racket");
     assert!(status.success(), "racket failed with status: {}", status);
     println!("cargo::rerun-if-changed=rust.rkt");
     println!("cargo::rerun-if-changed=api.rkt");
