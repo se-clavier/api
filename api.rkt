0cb7a5c05993769f03b89bebdd22dad69641bc47
Asdro Huang <40518249+asd-a@users.noreply.github.com>
2025-05-03 19:41:58 +0800

    feat(expire): add auth expire time (#40)

--- api.rkt
+++ api.rkt
@@ -2,6 +2,7 @@
 
 (define (api #:api api #:type type #:enum enum #:array array #:option option #:alias alias)
   (begin ; Common definitions
+    (alias 'TimeDate 'string) ; ISO 8601 time date
     (alias 'TimeWeek 'string) ; ISO 8601 week
     (alias 'TimeDiff 'string) ; ISO 8601 time difference
     (alias 'Id 'uint) ; universal-unique identifier for indexing
@@ -26,6 +27,7 @@
 
     (type 'Auth
       `[id Id]
+      `[expire TimeDate]
       `[roles Roles]
       `[signature string])
     ; Template for authenticated requests
